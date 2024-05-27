269973a3df56d917874d33ac103f709c2f6bc436
Timeshifts <minjea441@gmail.com>
2024-05-28 00:11:54 +0900

    Try to fit explosion circle to ground

--- main.py
+++ main.py
@@ -1,9 +1,43 @@
-import cv2, time, torch
+import cv2, time, torch, math
 import numpy as np
 import pyautogui
 import pygetwindow as gw
 from ultralytics import YOLO
 import datetime
+
+from pitch_estimation import estimate_pitch
+from pose_estimation import draw_ar
+
+def get_frame(resize=True):
+    windows = gw.getWindowsWithTitle('Minecraft')
+
+    if len(windows) == 0:
+        print('No Minecraft Window found!')
+        cv2.destroyAllWindows()
+        exit()
+
+    # Find the Minecraft window
+    minecraft_window = windows[0]
+
+    # Get the coordinates and size of the Minecraft window
+    x, y, width, height = minecraft_window.left, minecraft_window.top, minecraft_window.width, minecraft_window.height
+
+    # 창 테두리 제거
+    x += 10
+    y += 60
+    width -= 20
+    height -= 70
+
+    region = (x, y, width, height)
+
+    # 화면 캡처
+    frame = capture_screen(region)
+    
+    # OpenCV로 화면 크기 조정
+    if resize:
+        frame = cv2.resize(frame, (window_width, int(window_width * height / width)))
+
+    return frame
 
 def capture_screen(region=None):
     # 화면을 캡처하고 NumPy 배열로 변환
@@ -11,6 +45,44 @@ def capture_screen(region=None):
     screenshot = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
     return screenshot
 
+def draw_explosion(frame, box, pitch=10):
+    x1, y1, x2, y2 = box
+
+    # 플레이어의 각도에 따라 값이 크게 변하지 않는 너비를 기준으로 사용
+    creeper_real = (0.6, 1.7)  # 실제 크리퍼의 너비, 높이 (블록 단위)
+    creeper_width_pixels = x2 - x1  # 감지된 크리퍼의 너비 (픽셀 단위)
+    creeper_height_pixels = creeper_width_pixels / creeper_real[0] * creeper_real[1]
+    
+    # pitch_estimation.py 참고
+    # estimated_pitch, count = estimate_pitch(frame)
+    # pitch = np.clip(estimated_pitch, 10.0, 40.0)
+
+    # 카메라에서 크리퍼까지의 거리 계산
+    # 마인크래프트의 fov는 수직 각도만이 설정된 값으로 고정되므로, 크리퍼의 높이 사용
+    fov = 70
+    distance = (creeper_real[1] * frame.shape[0]) / (2 * creeper_height_pixels * np.tan(np.deg2rad(fov / 2)))
+
+    explosion_radius_blocks = 3  # 크리퍼 폭발 반경 (블록 단위)
+    explosion_radius_pixels_x = int((explosion_radius_blocks / creeper_real[0]) * creeper_width_pixels)
+    explosion_radius_pixels_y = int((frame.shape[0] - y2) / distance * explosion_radius_blocks)
+    warn_radius_blocks = 7  # 크리퍼 폭발 취소 반경 (블록 단위)
+    warn_radius_pixels_x = int((warn_radius_blocks / creeper_real[0]) * creeper_width_pixels)
+    warn_radius_pixels_y = int((frame.shape[0] - y2) / distance * warn_radius_blocks)
+
+    # 크리퍼 발 좌표 계산
+    center = (int((x1 + x2) / 2), int(y2))
+
+    ratio = (y2 - y1) / (x2 - x1)
+    print(ratio)
+
+    # 폭발 반경 표시
+    cv2.ellipse(frame, center, 
+                (explosion_radius_pixels_x, explosion_radius_pixels_y),
+                0, 0, 360, (0, 0, 255), 2)
+    cv2.ellipse(frame, center, 
+                (warn_radius_pixels_x, warn_radius_pixels_y),
+                0, 0, 360, (0, 255, 255), 2)
+    
 # OpenCV 창의 가로 크기
 window_width = 1920
 frame_delay = 1 / 30
@@ -27,36 +99,10 @@ def main():
     model = YOLO('creeper_model.pt')
     model.to(device)
 
-
     while True:
-        windows = gw.getWindowsWithTitle('Minecraft')
-
-        if len(windows) == 0:
-            print('No Minecraft Window found!')
-            cv2.destroyAllWindows()
-            exit()
-
-        # Find the Minecraft window
-        minecraft_window = windows[0]
-
-        # Get the coordinates and size of the Minecraft window
-        x, y, width, height = minecraft_window.left, minecraft_window.top, minecraft_window.width, minecraft_window.height
-
-        # 창 테두리 제거
-        x += 10
-        y += 60
-        width -= 20
-        height -= 70
-
-        region = (x, y, width, height)
-
         start_time = time.time()
 
-        # 화면 캡처
-        frame = capture_screen(region)
-        
-        # OpenCV로 화면 크기 조정
-        frame = cv2.resize(frame, (window_width, int(window_width * height / width)))
+        frame = get_frame()
 
         # YOLOv5 모델로 객체 탐지
         results = model(frame, verbose=False)
@@ -71,8 +117,17 @@ def main():
                 x1, y1, x2, y2 = box
                 if conf > min_conf:  # 신뢰도가 일정 값 이상일 때만 표시
                     label = f'{model.names[int(cls)]} {conf:.2f}'
-                    cv2.circle(frame, (int((x1 + x2) / 2), int(y2)), 15, (0, 0, 0), -1)
+                    draw_explosion(frame, box)
+                    
+                    # pose_estimation.py 참고
+                    # creeper_image = frame[int(y1):int(y2), int(x1):int(x2)]
+                    # creeper_image = draw_ar(creeper_image)
+                    # if creeper_image is not None:
+                    #    cv2.imshow('Another Detector', creeper_image)
+                    
                 if debug_mode:
+                    if label is None:
+                        label = f'{model.names[int(cls)]} {conf:.2f}'
                     if conf > min_conf:
                         cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 2)
                         cv2.putText(frame, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
@@ -80,7 +135,6 @@ def main():
                         cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 255), 2)
                         cv2.putText(frame, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 255), 2)
 
-        
 
         cv2.imshow('Creeper Detector', frame)
 
